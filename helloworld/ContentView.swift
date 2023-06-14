ed0219003e2961002512e795703c789a31794e93
Joe Chan <s9903262@live.hkmu.edu.hk>
2023-06-14 18:15:17 +0800

    2nd

--- helloworld/ContentView.swift
+++ helloworld/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
             Text("Change the world")
+            Text("Change the world again")
         }
         .padding()
     }
