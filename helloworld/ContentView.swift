e89465ea187c60dbed5372d7447d59e8c98e8f15
Joe Chan <s9903262@live.hkmu.edu.hk>
2023-06-14 17:50:25 +0800

    Initial Commit

---
+++ helloworld/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  helloworld
+//
+//  Created by Joe Chan on 14/6/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ helloworld/helloworldApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloworldApp.swift
+//  helloworld
+//
+//  Created by Joe Chan on 14/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct helloworldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
